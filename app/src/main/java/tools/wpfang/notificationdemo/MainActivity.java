0ab12384e23c0fdcc95556a23290cda4ef5c70c4
Wen-Pinn Fang <Wen_Pinn@hotmail.com>
2018-07-12 15:49:25 +0800

    test

--- app/src/main/java/tools/wpfang/notificationdemo/MainActivity.java
+++ app/src/main/java/tools/wpfang/notificationdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package tools.wpfang.notificationdemo;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
@@ -12,7 +13,8 @@ public class MainActivity extends AppCompatActivity {
     NotificationManager notificationManager;
     NotificationCompat.Builder builder;
     private static final int NOTIFICATION_ID=1;
-
+    private static final String NOTIFICATION_CHANNEL="Setting";
+    NotificationChannel notificationChannel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +22,19 @@ public class MainActivity extends AppCompatActivity {
         notificationManager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
+    public void createNewChannnel(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel;
+            channel = new NotificationChannel(NOTIFICATION_CHANNEL,"Setting", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("All the Setting Notificarion will come here");
+            channel.enableVibration(true);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
     public void doNotification(View view) {
-        builder=new NotificationCompat.Builder(this);
+        createNewChannnel();
+        builder=new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL);
         builder.setContentTitle("1 New message");
         builder.setContentText("Hi,How mare you");
         builder.setSmallIcon(R.mipmap.ic_launcher);
